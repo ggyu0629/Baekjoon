4bb2b80cd530da11f55c405831ac3bd44e175734
ggyu0629 <101187523+ggyu0629@users.noreply.github.com>
2023-05-09 05:17:53 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 12820 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        StringBuilder sb = new StringBuilder();
+        
         String s = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30";
-        Map<String, String> res = new HashMap<>();
 
         for (int i = 0; i < 28; i++) {
             s = s.replaceFirst(sc.nextLine(),"");
@@ -16,15 +16,17 @@ public class Main {
 
         s = s.trim();
 
-        System.out.print(s.charAt(0));
+        sb.append(s.charAt(0));
 
         if(s.charAt(1) != ' ') {
-            System.out.print(s.charAt(1));
+            sb.append(s.charAt(1));
         }
-        System.out.println();
+        sb.append("\n");
 
         s = s.substring(2);
         s = s.trim();
-        System.out.println(s);
+        sb.append(s);
+
+        System.out.println(sb);
     }
 }
