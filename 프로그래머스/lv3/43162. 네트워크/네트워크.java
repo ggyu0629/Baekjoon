c390daf63052d9ada0469592e4236bf7f7e38de3
ggyu0629 <101187523+ggyu0629@users.noreply.github.com>
2023-05-15 16:33:37 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visit = new boolean[n];
+        
+        Queue<int[]> q = new LinkedList<int[]>();
+        
+        for(int i=0; i<computers.length; i++) {
+            if(visit[i]){
+                continue;
+            }
+
+            q.add(computers[i]);
+            visit[i] = true;
+
+            while(!q.isEmpty()) {
+                int[] temp = q.poll();
+
+                for(int j=0; j<temp.length; j++) {
+                    if(!visit[j]) {
+                        if(temp[j] == 0)
+                            continue;
+                        
+                        q.add(computers[j]);
+                        visit[j] = true;
+                    }
+                }
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
