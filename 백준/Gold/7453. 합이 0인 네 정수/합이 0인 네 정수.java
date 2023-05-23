564a19fa59c8d266a2e9b861d09954a7d0baa06d
ggyu0629 <101187523+ggyu0629@users.noreply.github.com>
2023-05-24 03:33:08 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 2240 ms, Memory: 537924 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -3,36 +3,26 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    //합이 0인 네 정수
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         int n = Integer.parseInt(br.readLine());
-
         int[][] arr = new int[4][n];
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < 4; j++) 
                 arr[j][i] = Integer.parseInt(st.nextToken());
-            }
         }
-
         Arrays.sort(arr[0]);
         Arrays.sort(arr[1]);
         Arrays.sort(arr[2]);
         Arrays.sort(arr[3]);
-
         long[][] sumTwoArr = new long[2][n*n];
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
             for (int j = 0; j < n; j++) {
                 sumTwoArr[0][n*i + j] = arr[0][i] + arr[1][j];
                 sumTwoArr[1][n*i + j] = arr[2][i] + arr[3][j];
             }
-        }
-
         Arrays.sort(sumTwoArr[0]);
         Arrays.sort(sumTwoArr[1]);
 
@@ -48,7 +38,6 @@ public class Main {
             }else{
                 long highCnt = 1;
                 long lowCnt = 1;
-
                 while(highIndex != n*n - 1 ){
 
                     if(sumTwoArr[0][highIndex] == sumTwoArr[0][highIndex + 1]){
@@ -56,9 +45,7 @@ public class Main {
                         highIndex++;
                     }else
                         break;
-
                 }
-
                 while(lowIndex != 0){
                     if(sumTwoArr[1][lowIndex] == sumTwoArr[1][lowIndex -1]){
                         lowIndex--;
